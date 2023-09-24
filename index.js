7bc8c645a4e674b9ef99c7ff962be47e4989b05a
fibarrafdec <fibarrafdec@gmail.com>
2023-09-24 12:39:58 -0300

    add README file template

--- index.js
+++ index.js
@@ -11,3 +11,33 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+//THIS IS THE CODE FROM THE MINI-PROJECT 09.NODE JS.
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Enter your name:',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Enter your location:',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL:',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub URL:',
+    },
+  ])
